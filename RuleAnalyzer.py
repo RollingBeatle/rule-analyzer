77769debc8100f3ab22fef1b8f8acd1b21391a6b
Diego Ortiz <diego.ortiz.barbosa94@gmail.com>
2022-10-28 10:08:14 -0700

    basic skLearn implementation and export of csv file

--- RuleAnalyzer.py
+++ RuleAnalyzer.py
@@ -1,13 +1,18 @@
 import argparse
 import logging
 import os
+from pydoc import doc
 import pyfiglet
+import pandas as pd
+from models.WordBag import WordBag
 
 def openFile(filename):
-    lineArray = []
+    lines = []
     with open("./data/"+filename) as file:
-        lineArray = file.readlines()
-    return lineArray
+        lines = [line.rstrip('\n') for line in file]
+    return lines
+def export(frame, name):
+    frame.to_csv('./results/'+name+'.csv')
 
 if __name__ == "__main__":
     ascii_banner = pyfiglet.figlet_format("Natural Lang Processor")
@@ -17,8 +22,15 @@ if __name__ == "__main__":
     logging.basicConfig()
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', '-n', type=str, help='Name of data file')
+    parser.add_argument('--csv', '-c', type=str, help='Name of file to save')
     
     args = parser.parse_args()
 
     document = openFile(args.name)
+    print(document)
+
+    modelPL = WordBag(document)
+    modelPL.skImpl()
+    export(modelPL.res, args.csv)
+
 
--- models/WordBag.py
+++ models/WordBag.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
@@ -6,6 +7,17 @@ class WordBag:
     def __init__(self, data):
         self.data = data
         self.res = None
-    def cleanData():
-        return
+        self.clean = []
+    def cleanData(self):
+        for doc in self.data:
+            self.clean[doc] = re.sub(r"[^a-zA-Z0-9]", " ", self.data[doc].lower()).split()
+       
+    def skImpl(self):
+        vec = CountVectorizer (ngram_range=(2,2), stop_words="english")
+        fitData = vec.fit_transform(self.data)
+        df = pd.DataFrame(fitData.toarray(), columns=vec.get_feature_names())
+        self.res = df
+        print(df)
+
+
     
