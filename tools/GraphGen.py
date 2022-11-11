8ed0db10f027e4df772c26167fc699c12f8e9de2
Diego Ortiz <diego.ortiz.barbosa94@gmail.com>
2022-11-11 13:49:45 -0800

    scatter plot support, single line support

--- tools/GraphGen.py
+++ tools/GraphGen.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 class GraphGen:
     def __init__(self):
        pass
+    
     def barGraph(self, data, names, title):
         fig = plt.figure(figsize=(10,7))
         for i in range(0,len(data)):
@@ -13,7 +14,21 @@ class GraphGen:
         plt.xlabel('$Words$', fontsize=14)
         plt.xticks([])
         plt.title("$"+title+"$")
+
     def showResults(self):
         plt.show()
+
+    def lineGraphSingle (self, rangeX, rangeY, titleX, titleY):
+        plt.plot(rangeX, rangeY)
+        plt.xticks(rangeX)
+        plt.xlabel(titleX)
+        plt.ylabel(titleY)
+
+    def spreadGraph(self, coordX, coordY):
+        plt.scatter(coordY[:,0], coordY[:,1])
+        plt.scatter(coordX[:,0], coordX[:,1], c='black', s=300, alpha=0.6)
+        
+
+
 
     
