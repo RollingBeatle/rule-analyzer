0b321188dde110f2bee7cc32f6545a8d6748ba43
Diego Ortiz <diego.ortiz.barbosa94@gmail.com>
2022-11-11 13:06:53 -0800

    added elbow and silhouette tests

---
+++ models/KMeans.py
@@ -0,0 +1,49 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.metrics import silhouette_score
+
+
+class K_Means:
+    def __init__(self, data):
+        self.data = data
+
+    def vectorModel(self):
+        word_features = self.data.get_feature_names()
+        print(word_features)
+        stemmer = SnowballStemmer('english')
+        tokenizer = RegexpTokenizer(r'[a-zA-Z\']+')
+    
+    def KmeansModel(self, vector):
+        kmeans = KMeans(n_clusters = 4, n_init = 10)
+        
+        #print(self.data)
+        kmeans.fit(self.data)
+        print('hereeeeeeeeeeeeeeeeeeee')
+        print(kmeans.labels_)
+        words = vector.get_feature_names()
+        common_words = kmeans.cluster_centers_.argsort()[:,-1:-11:-1]
+        for num, centroid in enumerate(common_words):
+            print(str(num) + ' : ' + ', '.join(words[word] for word in centroid))
+
+    def elbowMeasure(self, rangeTest):
+        clusterR = []
+        for k in range(2,rangeTest):
+            kmeans = KMeans(n_clusters=k, init="random", n_init=10, max_iter=300)
+            kmeans.fit(self.data)
+            score = kmeans.inertia_
+            clusterR.append(score)
+        return clusterR
+
+    def silhoutteMeasure(self, rangeTest):
+        clusterR = []
+        for k in range(2,rangeTest):
+            kmeans = KMeans(n_clusters=k, init="random", n_init=10, max_iter=300)
+            kmeans.fit(self.data)
+            score = silhouette_score(self.data, kmeans.labels_)
+            clusterR.append(score)
+        return clusterR    
+    
