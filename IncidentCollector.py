bd622770e887644a18ebea29c0c3b0f862712c5c
Diego Ortiz <diego.ortiz.barbosa94@gmail.com>
2023-09-23 23:27:57 -0700

    separated functions

--- IncidentCollector.py
+++ IncidentCollector.py
@@ -5,8 +5,57 @@ from selenium.webdriver.common.by import By
 import time
 import random
 from urllib.parse import unquote
+import pandas as pd
+import os
 
+DataPd = None
 
+def saveCSV(savedArr):
+    df = pd.DataFrame(columns=['URL', 'visited'])
+    df['URL'] = savedArr
+    df['visited'] = 0
+    df.to_csv('./results/incidentsURL.csv')
+    global DataPd 
+    DataPd = df
+
+def readDownload():
+    df = pd.read_csv("./results/incidentsURL.csv")
+    
+    missingPDF = df[df['visited'] == 0].values
+
+    downloadNew(missingPDF)
+
+def downloadNew(ReportLinks):
+
+    counter = 0
+    for link in ReportLinks:
+        delay = random.uniform(1,5)
+        time.sleep(delay)
+        pdf_url= link
+        counter+=1
+        response = requests.get(pdf_url)
+        if response.status_code == 200:
+            content_disposition = response.headers.get('Content-Disposition')
+            if content_disposition:
+                # Split the header to find the filename part
+                parts = content_disposition.split(';')
+                for part in parts:
+                    if 'filename' in part:
+                        filename = part.split('=')[1].strip('" ')
+                        break
+                    else: 
+                        filename = "unlabledReport"+str(counter)+'.pdf'
+            else: 
+                filename = "unlabledReport"+str(counter)+'.pdf'
+            filename = unquote(filename)
+
+            content = response.content
+            with open(filename, "wb") as file:
+                file.write(content)
+            print('file saved')
+        else:
+            print(f'error code {response.status_code}')
+    
 
 def main():
     parser = argparse.ArgumentParser()
@@ -48,35 +97,23 @@ def main():
             except Exception as e:
                 break
     if args.save:
-            print("number of records "+str(len(data2)))
-            counter = 0
-            for link in ReportLinks:
-                delay = random.uniform(1,5)
-                time.sleep(delay)
-                pdf_url= link
-                counter+=1
-                response = requests.get(pdf_url)
-                if response.status_code == 200:
-                    content_disposition = response.headers.get('Content-Disposition')
-                    if content_disposition:
-                        # Split the header to find the filename part
-                        parts = content_disposition.split(';')
-                        for part in parts:
-                            if 'filename' in part:
-                                filename = part.split('=')[1].strip('" ')
-                                break
-                            else: 
-                                filename = "unlabledReport"+str(counter)+'.pdf'
-                    else: 
-                        filename = "unlabledReport"+str(counter)+'.pdf'
-                    filename = unquote(filename)
+            if os.path.exists("./results/incidentsURL.csv"):
+                option = str(input("file already exists, overwrite it? (Y/N) \n"))
+                if option == 'Y':
+                    saveCSV(ReportLinks)
+                else: 
+                    download = input("Continue downloading from file? (Y/N)")
+                    if download == "Y":
+                        readDownload()
+                    else:    
+                        return
+            else:
+                print("number of records "+str(len(ReportLinks)))
+                saveCSV(ReportLinks)
+                downloadNew(ReportLinks)
 
-                    content = response.content
-                    with open(filename, "wb") as file:
-                        file.write(content)
-                    print('file saved')
-                else:
-                    print(f'error code {response.status_code}')
+         
+            
         
     
 
